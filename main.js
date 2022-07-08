75e35a56e1f924859a5d7bf097a367fcc0291f69
Okan <97271658+okanzkaya@users.noreply.github.com>
2022-07-08 10:49:11 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,16 +1,18 @@
-function toggleNav(){
-    
-    var x = document.getElementById("navbar-menu");
+function toggleNav() {
+
+    var navbar = document.getElementById("navbar-menu");
     var body = document.getElementsByTagName("BODY")[0];
-    if(window.getComputedStyle(x).display != "none"){
-        x.style.animationName = "navbar-menu-closure";
-        setTimeout(function(){
-            x.style.display = "none";
+    if (window.getComputedStyle(navbar).display != "none") {
+        navbar.style.animationName = "navbar-menu-closure";
+        setTimeout(function () {
+            navbar.style.display = "none";
             body.style.overflowY = "visible";
         }, 470)
-        }
-    else{x.style.animationName = "navbar-menu";
-    x.style.display = "block";
-    body.style.overflowY = "hidden";}
+    }
+    else {
+        navbar.style.animationName = "navbar-menu";
+        navbar.style.display = "block";
+        body.style.overflowY = "hidden";
+    }
 }
 
